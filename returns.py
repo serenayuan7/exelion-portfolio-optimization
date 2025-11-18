082dc09d10997222ee099d60653e1cc232de0cf8
serenayuan7 <serenay2567@gmail.com>
2025-11-17 20:00:57 -0800

    Create returns.py

---
+++ returns.py
@@ -0,0 +1,15 @@
+def get_returns(prices):
+  n = len(prices)
+  returns = []
+  for i in range(1,n):
+    ret = (p[i] - p[i-1])/p[i-1]
+    returns.append(ret)
+  return returns
+
+def get_log_returns(prices):
+  n = len(prices)
+  returns = []
+  for i in range(1, n):
+    logret = np.log(prices[i]) - np.log(prices[i-1])
+    returns.append(logret)
+  return returns
