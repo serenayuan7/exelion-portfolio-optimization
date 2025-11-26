87f493e34887e552bbdd51cd0aa4c72747af1414
serenayuan7 <serenay2567@gmail.com>
2025-11-25 16:22:45 -0800

    Create rsi.py

---
+++ rsi.py
@@ -0,0 +1,19 @@
+def rsi(prices):
+  # average gain
+  again = []
+  aloss = []
+  n = len(prices) # number of periods
+  for i in range(n-1):
+    pnext = prices[i+1]
+    pnow = prices[i]
+    if pnext> pnow:
+      again.append(pnext- pnow)
+    else:
+      aloss.append(pnow - pnext)
+  gains = np.sum(again)
+  losses = np.sum(aloss)
+  ag = gains/n
+  al = losses/n
+  rel_strength = ag/al
+  rsi = 100 - (100/(1+rel_strength))
+  return rsi
